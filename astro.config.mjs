3677039c618ad3203905de6e4cad3cf2a4fdf18f
somethingxdev <hevnxv19@gmail.com>
2024-09-05 16:44:21 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({applyBaseStyles: false}), sitemap()],
-  output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'hybrid',
+  adapter: vercel()
 });
