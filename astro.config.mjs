67aca695a45be5dc03cfdf7e35ac482e742d338d
somethingxdev <hevnxv19@gmail.com>
2024-09-05 16:47:08 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
-
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
